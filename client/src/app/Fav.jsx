bc76bf5b43533e04ad5ecde35541c5f0e942886c
todendvv91 <todendvv91@gmail.com>
2023-10-16 15:04:00 +0900

    test

--- client/src/app/Fav.jsx
+++ client/src/app/Fav.jsx
@@ -1,9 +1,13 @@
 import Layout from "../components/Layout";
+import useUser from "../hooks/useUser";
 
 export default function Fav() {
+  const {user} = useUser();
+  const missions = user?.missionCompleted;
+  console.log(missions);
   return (
     <Layout isFooter>
-      <div>fav</div>
+      <div className="py-16 px-4">fav</div>
     </Layout>
   );
 }
--- client/src/app/Qr.jsx
+++ client/src/app/Qr.jsx
@@ -2,14 +2,33 @@ import { useState } from "react";
 import Layout from "../components/Layout";
 import { QrScanner } from "@yudiel/react-qr-scanner";
 import { GridLoader } from "react-spinners";
+import useUser from "../hooks/useUser";
+import { useMutation } from "react-query";
+import { missinComplated } from "../api";
 
 export default function Qr() {
   const [prevResult, setPrevResult] = useState(null);
+  const {user} = useUser();
+  const userId = user?.id
+
+  const {mutate} = useMutation(missinComplated, {
+    onSuccess: (data) => {
+      if(data.result === 0){
+        alert(data.message);
+      }else if (data.result === 1){
+        alert(data.message);
+      }else if (data.result === 2){
+        alert(data.message);
+      }else{
+        return;
+      }
+    }
+  });
 
   const handleSuccess = (result) => {
     if (prevResult === null || prevResult !== result) {
       setPrevResult(result);
-      alert(prevResult);
+      mutate({missionId : result, userId});
       //  mutate 전송 react-query
     }
   };
